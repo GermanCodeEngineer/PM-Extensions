0fcd097d98fa196f6c7b9d84d80ba6130ca3254f
GermanCodeEngineer <GermanCodeEngineer@proton.me>
2025-10-25 00:04:29 +0200

    continued

--- extensions/classes.js
+++ extensions/classes.js
@@ -2,6 +2,7 @@
  * Author: GermanCodeEngineer
  * Credit: Inspired by & Based on
  *     - https://github.com/PenguinMod/PenguinMod-Vm/blob/develop/src/extensions/jg_scripts/index.js
+ *     - https://github.com/PenguinMod/PenguinMod-ExtensionsGallery/blob/main/static/extensions/DogeisCut/dogeiscutObject.js
  *     - https://github.com/PenguinMod/PenguinMod-ExtensionsGallery/blob/main/static/extensions/VeryGoodScratcher42/More-Types.js
  */
 
@@ -14,6 +15,10 @@ if (!Scratch.extensions.unsandboxed) {
 
 // Helper Classes
 class Script {
+    /**
+     * @param {string} name 
+     * @param {Array} blocks 
+     */
     constructor(name, blocks) {
         this.name = name
         this.blocks = blocks
@@ -26,9 +31,34 @@ class Script {
     }
 }
 
+class Method {
+    /**
+     * @param {string} name
+     * @param {Script} script 
+     * @param {Array<string>} argNames
+     * @param {Array<string>} argDefaults
+     */
+    constructor(name, script, argNames, argDefaults) {
+        this.name = name
+        this.script = script
+        this.argNames = argNames
+        this.argDefaults = argDefaults
+    }
+    toString() {
+        return `<Method '${this.name}'`
+    }
+    toJSON() {
+        return "Methods do not save."
+    }
+}
+
 class CustomClass {
+    /**
+     * @param {string} name 
+     */
     constructor(name) {
         this.name = name
+        this.methods = {}
     }
     toString() {
         return `<Custom Class '${this.name}'>`
@@ -37,6 +67,23 @@ class CustomClass {
         return "Custom Classes do not save."
     }
 }
+
+class CustomClassInstance {
+    /**
+     * @param {CustomClass} cls 
+     */
+    constructor(cls) {
+        this.cls = cls
+        this.attributes = {}
+    }
+    toString() {
+        return `<Instance of '${this.cls.name}'>`
+    }
+    toJSON() {
+        return "Custom Class Instances do not save."
+    }
+}
+
 
 class VariableManager {
     constructor() {
@@ -105,39 +152,106 @@ class VariableManager {
 }
 
 const BlockType = Scratch.BlockType
+const BlockShape = Scratch.BlockShape
 const ArgumentType = Scratch.ArgumentType
 const Cast = Scratch.Cast
 const runtime = Scratch.vm.runtime
 const vars = new VariableManager()
 
-// Add Array Extension
-//if (!Scratch.vm.jwArray) Scratch.vm.extensionManager.loadExtensionIdSync("jwArray")
-//const jwArray = Scratch.vm.jwArray
+// Add required extensions
+Scratch.vm.extensionManager.loadExtensionIdSync("jwArray")
+if (!Scratch.vm.dogeiscutObject) (async () => {
+    await Scratch.vm.extensionManager.loadExtensionURL("https://extensions.penguinmod.com/extensions/DogeisCut/dogeiscutObject.js");
+})();
+
+
+// Types
+const jwArray = {
+    Type: null,
+    Block: {
+        blockType: BlockType.REPORTER,
+        blockShape: BlockShape.SQUARE,
+        forceOutputType: "Array",
+        //allowDropAnywhere: true,
+        disableMonitor: true
+    },
+    Argument: {
+        shape: BlockShape.SQUARE,
+        exemptFromNormalization: true,
+        check: ["Array"],
+        compilerInfo: {
+            jwArrayUnmodified: true
+        }
+    },
+}
+const dogeiscutObject = {
+    Type: null,
+    Block: {
+        blockType: BlockType.REPORTER,
+        blockShape: BlockShape.PLUS,
+        forceOutputType: "Object",
+        disableMonitor: true
+    },
+    Argument: {
+        shape: BlockShape.PLUS,
+        exemptFromNormalization: true,
+        check: ["Object"]
+    }
+}
+const gceClassInstance = {
+    Type: null, // TODO: eventually create
+    Block: {
+        blockType: BlockType.REPORTER,
+        blockShape: BlockShape.BUMPED,
+        //forceOutputType: "Object",
+        disableMonitor: true
+    },
+    Argument: {
+        shape: BlockShape.BUMPED,
+        exemptFromNormalization: true,
+        //check: ["Object"]
+    }
+}
+
 
 class Extension {
     constructor() {
-        /**
-         * The runtime instantiating this block package.
-         * @type {Runtime}
-         */
+        this.reset()
         // TODO: possibly remove? // TODO: change on release
-        runtime.on("PROJECT_START", () => {
-            vars.reset();
-            console.clear()
-        })
-        runtime.on("PROJECT_STOP_ALL", vars.reset)
+        runtime.on("PROJECT_START", this.reset)
+        runtime.on("PROJECT_STOP_ALL", this.reset)
+    }
+
+    reset() {
+        // block id => Custom Class
+        this.blockClasses = {}
+        this.resetNextMethodArgConfig()
+        vars.reset()
+        console.clear() // TODO: remove on release
+    }
+
+    resetNextMethodArgConfig() {
+        this.nextMethodArgConfig = {names: [], defaults: []}
     }
 
     /**
      * @returns {object} metadata for this extension and its blocks.
      */
     getInfo() {
+        const makeLabel = (text) => {
+            return {
+            blockType: Scratch.BlockType.LABEL,
+            text: text
+            }
+        }
         return {
             id: "gceClasses",
             name: "Classes",
             color1: "#d9661a",
             color2: "#b34801",
-            blocks: [{
+            blocks: [
+                makeLabel("Variables n Stuff"),
+                {
                     opcode: "getVar",
                     blockType: BlockType.REPORTER,
                     text: "get variable [NAME]",
@@ -167,12 +281,14 @@ class Extension {
                 {
                     opcode: "allScripts",
                     blockType: BlockType.REPORTER,
-                    text: "all scripts"
+                    text: "all scripts",
+                    disableMonitor: true,
                 },
                 {
                     opcode: "allClasses",
                     blockType: BlockType.REPORTER,
-                    text: "all classes"
+                    text: "all classes",
+                    disableMonitor: true,
                 },
                 {
                     opcode: "varExists",
@@ -207,6 +323,7 @@ class Extension {
                     disableMonitor: true
                 },
                 "---",
+                makeLabel("Running Scripts"),
                 {
                     opcode: "runBlocks",
                     text: "run script [NAME] in [SPRITE]",
@@ -243,7 +360,6 @@ class Extension {
                         }
                     },
                 },
-                "---",
                 {
                     opcode: "reportBlocks",
                     text: "run script [NAME] in [SPRITE]",
@@ -281,11 +397,11 @@ class Extension {
                     },
                 },
                 "---",
-                "---",
+                makeLabel("Class Creation"),
                 // New
                 {
                     opcode: "createClass",
-                    blockType: BlockType.COMMAND,
+                    blockType: BlockType.CONDITIONAL,
                     text: ["create class [NAME]"],
                     branchCount: 1,
                     arguments: {
@@ -295,11 +411,51 @@ class Extension {
                         }
                     },
                 },
-
+                {
+                    opcode: "configureNextMethodArguments",
+                    blockType: BlockType.COMMAND,
+                    text: "configure next method with argument names [ARGNAMES] defaults [ARGDEFAULTS]",
+                    arguments: {
+                        ARGNAMES: jwArray.Argument,
+                        ARGDEFAULTS: jwArray.Argument,
+                    }
+                },
+                {
+                    opcode: "defineMethod",
+                    blockType: BlockType.CONDITIONAL,
+                    text: ["define method [NAME]"],
+                    branchCount: 1,
+                    arguments: {
+                        NAME: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "myMethod"
+                        },
+                    },
+                },
+                {
+                    opcode: "methodArgs",
+                    text: "method arguments",
+                    ...dogeiscutObject.Block,
+                },
+                "---",
+                makeLabel("Class Usage"),
+                {
+                    opcode: "createInstance",
+                    text: "create instance of class [NAME]",
+                    arguments: {
+                        NAME: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "MyClass"
+                        },
+                    },
+                    ...gceClassInstance.Block
+                },
+                "---",
+                makeLabel("Script Creation"),
                 {
                     opcode: "createScriptFromBranch",
-                    blockType: BlockType.COMMAND,
-                    text: ["create script named [NAME]"],
+                    blockType: BlockType.CONDITIONAL,
+                    text: ["create script [NAME]"],
                     branchCount: 1,
                     arguments: {
                         NAME: {
@@ -308,6 +464,15 @@ class Extension {
                         }
                     },
                 },
+                "---",
+                makeLabel("Temporary"),
+                {
+                    opcode: "typeof",
+                    text: "typeof [VALUE] [X]",
+                    blockType: BlockType.REPORTER,
+                    arguments: {VALUE: {type: ArgumentType.STRING, exemptFromNormalization: true}, X: gceClassInstance.Argument},
+                },
+
             ],
             menus: {
                 TARGETS: {
@@ -319,6 +484,7 @@ class Extension {
     }
 
     // Helpers
+
     _getTargets() {
         const spriteNames = [{
                 text: "myself",
@@ -340,8 +506,7 @@ class Extension {
         return spriteNames.length > 0 ? spriteNames : [""]
     }
 
-    _createScriptFromBranch(name, util) {
-        vars.assertFree(name)
+    _createScriptFromBranch(util, name) {
         const script = new Script(name, [])
         const branch = util.thread.target.blocks.getBranch(util.thread.peekStack(), 1)
         if (branch) {
@@ -350,7 +515,7 @@ class Extension {
                 target: util.target
             })
         }
-        vars.addScript(name, script)
+        return script
     }
 
     _getMenuTarget(sprite, util) {
@@ -368,7 +533,6 @@ class Extension {
 
         // Prepare stack frame and get thread
         const frame = util.stackFrame
-        console.log("util", util)
         if (frame.JGindex === undefined) frame.JGindex = 0
         if (frame.JGthread === undefined) frame.JGthread = ""
         const blocks = vars.getScript(name).blocks
@@ -390,6 +554,32 @@ class Extension {
         }
 
         return [frame, blocks] // For later use
+    }
+
+    /**
+     * @return {CustomClass}
+     */
+    _getContainerClass(ownId, blockContainer) {
+        let currentId = ownId
+        let parentId = null
+        let currentBlock = null
+        let parentBlock = null
+        let finalId = null
+
+        while (true) {
+            currentBlock = blockContainer.getBlock(currentId)
+            parentId = currentBlock.parent
+            if (parentId === null) break
+            parentBlock = blockContainer.getBlock(parentId)
+            if ((parentBlock.next !== currentId) && (parentBlock.opcode === "gceClasses_createClass")) {
+                // this ensures the parent block is to the left and not above the current block
+                finalId = parentId
+                break
+            }
+            currentId = parentId
+        }
+        if (finalId === null) return null
+        else return this.blockClasses[finalId]
     }
 
 
@@ -434,7 +624,7 @@ class Extension {
         this.runBlocks(args, util)
     }
     runBlocks(args, util) {
-        const [frame, blocks] = this._prepareRun(args, util);
+        const [frame, blocks] = this._prepareRun(args, util)
         if (frame === null) return
 
         // Run the thread if it is active, otherwise clean up
@@ -447,7 +637,7 @@ class Extension {
         return this.reportBlocks(args, util) || ""
     }
     reportBlocks(args, util) {
-        const [frame, blocks] = this._prepareRun(args, util);
+        const [frame, blocks] = this._prepareRun(args, util)
         if (frame === null) return
 
         // Run the thread if it is active, otherwise set return value and clean up
@@ -465,15 +655,68 @@ class Extension {
 
     // New Blocks
 
-    createClass(args, util) {
+    createClass(args, util, blockInfo) {
         const name = Cast.toString(args.NAME)
         vars.assertFree(name)
         const cls = new CustomClass(name)
+        const ownId = util.thread.peekStack()
+
+        this.blockClasses[ownId] = cls
         vars.addClass(name, cls)
+        util.startBranch(1, false)
+    }
+
+    configureNextMethodArguments(args, util) {
+        this.resetNextMethodArgConfig()
+        if (args.ARGNAMES?.customId === "jwArray") {
+            args.ARGNAMES.array.forEach(argName => {
+                this.nextMethodArgConfig.names.push(Cast.toString(argName))
+            });
+        }
+        if (args.ARGDEFAULTS?.customId === "jwArray") {
+            args.ARGDEFAULTS.array.forEach(argDefault => {
+                this.nextMethodArgConfig.defaults.push(Cast.toString(argDefault))
+            });
+        }
+        if (this.nextMethodArgConfig.defaults.length > this.nextMethodArgConfig.names.length) {
+            this.resetNextMethodArgConfig()
+            throw new Error("there can only be as many defaults as argument names")
+        }
+    }
+
+    defineMethod(args, util) {
+        const name = Cast.toString(args.NAME)
+        const ownId = util.thread.peekStack()
+        const blockContainer = util.thread.blockContainer
+        const containerClass = this._getContainerClass(ownId, blockContainer)
+        if (containerClass === null) throw new Error("'add method' must be used within a class")
+        
+        const methodArgConfig = this.nextMethodArgConfig
+        this.resetNextMethodArgConfig()
+        const script = this._createScriptFromBranch(util, "<anonymous>")
+        containerClass.methods[name] = new Method(name, script, methodArgConfig.names, methodArgConfig.defaults)
+        console.log(containerClass)
+    }
+
+    createInstance(args, util) {
+        const name = Cast.toString(args.NAME)
+        return new CustomClassInstance(vars.getClass(name))
+    }
+
+    methodArgs(args, util) {
+        // Step 2
     }
 
     createScriptFromBranch(args, util) {
-        this._createScriptFromBranch(Cast.toString(args.NAME), util)
+        const name = Cast.toString(args.NAME)
+        vars.assertFree(name)
+        const script = this._createScriptFromBranch(util, name)
+        vars.addScript(name, script)
+    }
+
+    typeof(args, util) {
+        console.log("value", args.VALUE)
+        console.log("typeof value", typeof args.VALUE)
     }
 }
 
