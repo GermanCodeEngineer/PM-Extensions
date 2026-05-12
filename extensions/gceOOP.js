37359077ed8994df67f4f28cdcc9b7b33d0a9280
GermanCodeEngineer <germancodeengineer@proton.me>
2026-05-12 15:36:15 +0200

    Continue refractor

--- extensions/gceOOP.js
+++ extensions/gceOOP.js
@@ -297,10 +297,19 @@ function createCustomShape(ScratchBlocks) {
             // My New Shape
             // Inspired by https://github.com/PenguinMod/PenguinMod-ExtensionsGallery/blob/main/static/extensions/Div/divIterators.js
             
-            SHAPE.emptyInputPath = `m 31 0 h -15 q -3 0 -5 2 l -8 8 q -3 3 -3 4 v 4 q 0 1 3 4 l 8 8 q 2 2 5 2 h 15 h 11 c 2 0 3 0 4 -1 s 1 -3 0 -4 l -9 -9 v -4 l 8 -8 c 2 -2 2 -4 1 -5 s -2 -1 -4 -1 h -11 z`
+            SHAPE.emptyInputPath = `m 31 0 h -15 ` +
+                `q -3 0 -5 2 l -8 8 ` +
+                `q -3 3 -3 4 v 4 ` +
+                `q 0 1 3 4 l 8 8 ` +
+                `q 2 2 5 2 h 15 ` +
+                `h 11 c 2 0 3 0 4 -1 s 1 -3 0 -4 ` +
+                `l -9 -9 v -4 ` +
+                `l 8 -8 c 2 -2 2 -4 1 -5 s -2 -1 -4 -1 ` +
+                `h -11 z`
 
             
             SHAPE.leftPath = (block) => {
+                console.log("leftPath reveiving block", block)
                 let h
                 if (block.inputList.some(i => i.type === ScratchBlocks.NEXT_STATEMENT)) {
                     h = -14.5
@@ -308,29 +317,41 @@ function createCustomShape(ScratchBlocks) {
                     const edgeWidth = block.height / 2
                     h = -2 * Math.max(edgeWidth - 14*1.25, 0)
                 }
-                
+
                 const hHalf = h / 2.0
                 const hHalfOffset = hHalf - 13.75
                 const negHHalfOffset = - hHalfOffset
                 
                 return [
-                    `h ${hHalfOffset} c -2.5 0 -3.75 0 -5 -1.25 s -1.25 -3.75 0 -5 l 11.25 -11.25 v ${h} l -10 -10 c -2.5 -2.5 -2.5 -5 -1.25 -6.25 s 2.5 -1.25 5 -1.25 h ${negHHalfOffset}`
+                    `h ${hHalfOffset} ` +
+                    `c -2.5 0 -3.75 0 -5 -1.25 ` +
+                    `s -1.25 -3.75 0 -5 ` +
+                    `l 11.25 -11.25 ` +
+                    `v ${h} ` +
+                    `l -10 -10 ` +
+                    `c -2.5 -2.5 -2.5 -5 -1.25 -6.25 ` +
+                    `s 2.5 -1.25 5 -1.25 ` +
+                    `h ${negHHalfOffset}`
                 ]
             }
 
             SHAPE.rightPath = (block) => {
+                console.log("rightPath reveiving block", block)
                 const edgeWidth = /*block.height/2.*/ block.edgeShapeWidth_
                 const h = 2*Math.max(edgeWidth - 14*1.25, 0)
                 const hHalf = h / 2.0
                 const negHHalf = -hHalf
                 return [
-                    `h ${hHalf} q 3.75 0 6.25 2.5 l 10 10 q 3.75 3.75 3.75 5 v ${h} q 0 1.25 -3.75 5 l -10 10 q -2.5 2.5 -6.25 2.5 h ${negHHalf}`
+                    `h ${hHalf} ` +
+                    `q 3.75 0 6.25 2.5 ` +
+                    `l 10 10 ` +
+                    `q 3.75 3.75 3.75 5 ` +
+                    `v ${h} ` +
+                    `q 0 1.25 -3.75 5 ` +
+                    `l -10 10 ` +
+                    `q -2.5 2.5 -6.25 2.5 ` +
+                    `h ${negHHalf}`
                 ]
-            }
-
-            SHAPE.outputLeftPadding = (block) => {
-                return block.inputList.some(i => i.type == ScratchBlocks.NEXT_STATEMENT)
-                ? -block.height/2 + 22 : 0
             }
 
             return SHAPE
